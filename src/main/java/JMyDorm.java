f940504e908d874684003e1b4f53379254d85aca
elisey654321 <elisey654321@mail.ru>
2022-06-28 18:06:05 +0300

    update_Finder_6

---
+++ src/main/java/JMyDorm.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class JMyDorm {
+    private JButton button1;
+}
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,9 +1,10 @@
+import javax.swing.*;
+
 public class test {
 
-    public static String testStr = "81079697163350";
-
     public static void main(String[] args) {
-        System.out.println(testStr.length());
+        JFrame frame = new JFrame("JMyDorm");
+//        frame.set
     }
 
 }
