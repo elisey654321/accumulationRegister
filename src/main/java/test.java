9b72688a60fa194565f88706eab6c6e935ae4737
elisey654321 <elisey654321@mail.ru>
2022-06-29 10:14:25 +0300

    Add form javafx.

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,10 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class test {
+public class test{
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("JMyDorm");
-//        frame.set
+        JFrame frame = new JFrame("myWindow");
+        Container container = new Container();
+        JButton button = new JButton("Please dont touch!");
+        ActionListener actionListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("You touch button.");
+            }
+        };
+        button.addActionListener(actionListener);
+
+        frame.add(button);
+
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
 }
